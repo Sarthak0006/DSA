fbeb4c740777b0740d66e456294990476f4ab50d
Sarthak <117128614+Sarthak0006@users.noreply.github.com>
2024-08-31 10:38:13 +0530

    Update Patterns.java

--- src/Patterns.java
+++ src/Patterns.java
@@ -16,5 +16,83 @@ public class Patterns {
             System.out.print("\n");
         }
     }
+
+     public static void Pattern2(int a){
+        /* *
+           **
+           ***
+           **** */
+        for ( int i = 0; i < a; i++) {
+            for ( int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+     public static void Pattern3(int a){
+        /* 1
+           12
+           123
+           1234 */
+        for ( int i = 0; i < a; i++) {
+            for ( int j = 0; j < a; j++) {
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+        }
+    }
+
+      public static void Pattern4(int a){
+        /* 1
+           22
+           333
+           4444 */
+        for ( int i = 0; i < a; i++) {
+            for ( int j = 0; j < a; j++) {
+                System.out.print(i+1);
+            }
+            System.out.print("\n");
+        }
+    }
+
+      public static void Pattern5(int a){
+        /* ****
+           ***
+           **
+           *  */
+        for ( int i = a; i > 0 ; i--) {
+            for ( int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+      public static void Pattern6(int a){
+        /* 1234
+           123
+           12
+           1 */
+        for ( int i = a; i > 0; i--) {
+            for ( int j = 0; j < i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+        }
+    }
+     
+      public static void Pattern6(int a){
+        /* ****
+           ****
+           ****
+           **** */
+        for ( int i = 0; i < a; i++) {
+            for ( int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
 }
 
